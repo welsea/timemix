6cd66c38328f22e8dbbd7862b8a761655cee5278
welsea <welsea.42@gmail.com>
2022-07-31 22:45:28 +0200

    remove

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b01a9268c1260e31"; }
+/******/ 		__webpack_require__.h = function() { return "0798977fdd63043d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
