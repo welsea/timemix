6b9955ef127e8bacb03649ae8c6f1f593650b723
welsea <welsea.42@gmail.com>
2022-08-08 15:37:55 +0200

    add ioredis

---
+++ pages/api/incr.js
@@ -0,0 +1,9 @@
+
+import Redis from 'ioredis'
+
+let redis = new Redis(process.env.REDIS_URL)
+
+export default async (req, res) => {
+  const count = await redis.incr('counter')
+  res.status(200).json({ count })
+}
